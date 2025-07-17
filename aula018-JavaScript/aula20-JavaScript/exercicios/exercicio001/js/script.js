e00babdbc85c92944928bb84121726cb0a596d2e
Davi-Melo-Ferreira <davidemeloferreira13@gmail.com>
2025-07-16 21:38:03 -0300

    exercicio

--- aula018-JavaScript/aula20-JavaScript/exercicios/exercicio001/js/script.js
+++ aula018-JavaScript/aula20-JavaScript/exercicios/exercicio001/js/script.js
@@ -1,1 +1,17 @@
-let subinoonibus = prompt("Digite uma frase");
+let texto = prompt("Digite um texto(palíndromo ou não)");
+
+let textolimpo = texto.replace(/ /g,'');
+// / / g encontra o que tiver inserido aqui / / e recoloca o que tiver dentro da " "
+
+let invertida = texto.split('').reverse().join('');
+// split = ['a','b','c']
+// reverse = ['c','b','a']
+// join = 'abc'
+
+
+// Comparação manual dos extremos
+if (invertida === texto){
+    alert("PALÍNDROMO");
+} else{
+    console.log("Não é um palíndromo");
+}
