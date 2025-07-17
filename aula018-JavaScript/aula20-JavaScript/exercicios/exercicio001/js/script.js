a1b648f8d46a3793742c224c2fbfd6da52d1ceec
Davi-Melo-Ferreira <davidemeloferreira13@gmail.com>
2025-07-16 21:43:45 -0300

    correcao-da-minha-burrice

--- aula018-JavaScript/aula20-JavaScript/exercicios/exercicio001/js/script.js
+++ aula018-JavaScript/aula20-JavaScript/exercicios/exercicio001/js/script.js
@@ -1,16 +1,16 @@
 let texto = prompt("Digite um texto(palíndromo ou não)");
 
-let textolimpo = texto.replace(/ /g,'');
-// / / g encontra o que tiver inserido aqui / / e recoloca o que tiver dentro da " "
+let textolimpo = texto.toLowerCase().replace(/ /g,'');
+// removendo espaços
 
-let invertida = texto.split('').reverse().join('');
+let invertida = textolimpo.split('').reverse().join('');
 // split = ['a','b','c']
 // reverse = ['c','b','a']
 // join = 'abc'
 
 
 // Comparação manual dos extremos
-if (invertida === texto){
+if (invertida === textolimpo){
     alert("PALÍNDROMO");
 } else{
     console.log("Não é um palíndromo");
