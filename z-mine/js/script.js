53fe9731a7800a379e41398d4b1b2d2c85ac07d4
Davi-Melo-Ferreira <davidemeloferreira13@gmail.com>
2025-08-21 21:05:35 -0300

    exercicio finalizado

---
+++ z-mine/js/script.js
@@ -0,0 +1,17 @@
+let cama = [
+  [null, null, null],
+  ['la', 'la', 'la'],
+  ['wood', 'wood', 'wood']
+];
+
+let madeiraFina = [
+  [null, null, null],
+  [null, null, null],
+  ['wood', 'wood', 'wood']
+];
+
+let grid = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null]
+];
